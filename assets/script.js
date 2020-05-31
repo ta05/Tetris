a8f210ecfc0aad226020549876b2c1cf4f69c1ef
Tolu Alimi <tolualimi05@gmail.com>
2020-05-31 12:12:33 -0500

    Added draw and undraw functions for Tetromino blocks

--- assets/script.js
+++ assets/script.js
@@ -4,52 +4,95 @@ const gridHeight = 20;
 createGridDivs(gridWidth, gridHeight);
 
 $(document).ready(function () {
+    let currentPosition = 4;
+    
     const grid = $(".grid");
+    let squares = Array.from(grid.find('div'));
 
     //Tetrominoes
 
     const lTetromino = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2]
+        [1, gridWidth + 1, gridWidth * 2 + 1, 2],
+        [gridWidth, gridWidth + 1, gridWidth + 2, gridWidth * 2 + 2],
+        [1, gridWidth + 1, gridWidth * 2 + 1, gridWidth * 2],
+        [gridWidth, gridWidth * 2, gridWidth * 2 + 1, gridWidth * 2 + 2]
     ];
 
     const zTetromino = [
-        [0, width, width+ 1, width * 2 + 1],
-        [width + 1, width + 2, width * 2, width * 2 + 1],
-        [0, width, width+ 1, width * 2 + 1],
-        [width + 1, width + 2, width * 2, width * 2 + 1]
+        [0, gridWidth, gridWidth+ 1, gridWidth * 2 + 1],
+        [gridWidth + 1, gridWidth + 2, gridWidth * 2, gridWidth * 2 + 1],
+        [0, gridWidth, gridWidth+ 1, gridWidth * 2 + 1],
+        [gridWidth + 1, gridWidth + 2, gridWidth * 2, gridWidth * 2 + 1]
     ];
 
     const tTetromino = [
-        [1, width, width + 1, width * 2],
-        [1, width + 1, width + 2, width * 2 + 2],
-        [width, width + 1, width + 2, width * 2 + 1],
-        [1, width, width + 1, width * 2 + 1]
+        [1, gridWidth, gridWidth + 1, gridWidth * 2],
+        [1, gridWidth + 1, gridWidth + 2, gridWidth * 2 + 2],
+        [gridWidth, gridWidth + 1, gridWidth + 2, gridWidth * 2 + 1],
+        [1, gridWidth, gridWidth + 1, gridWidth * 2 + 1]
     ];
 
     const oTetromino = [
-        [0, 1, width, width + 1],
-        [0, 1, width, width + 1],
-        [0, 1, width, width + 1],
-        [0, 1, width, width + 1]
+        [0, 1, gridWidth, gridWidth + 1],
+        [0, 1, gridWidth, gridWidth + 1],
+        [0, 1, gridWidth, gridWidth + 1],
+        [0, 1, gridWidth, gridWidth + 1]
     ];
 
     const iTetromino = [
-        [1, width + 1, width * 2 + 1, width * 3 + 1],
-        [width, width + 1, width + 2, width + 3],
-        [1, width + 1, width * 2 + 1, width * 3 + 1],
-        [width, width + 1, width + 2, width + 3]
+        [1, gridWidth + 1, gridWidth * 2 + 1, gridWidth * 3 + 1],
+        [gridWidth, gridWidth + 1, gridWidth + 2, gridWidth + 3],
+        [1, gridWidth + 1, gridWidth * 2 + 1, gridWidth * 3 + 1],
+        [gridWidth, gridWidth + 1, gridWidth + 2, gridWidth + 3]
     ];
 
-    const theTerominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
 
     // Randomly selects Tetromino
 
-    let random = Math.floor(Math.random() * theTerominoes.length);
-    
+    let random = Math.floor(Math.random() * theTetrominoes.length);
+    let currentRotation = Math.floor(Math.random() * 4);
+    let current = theTetrominoes[random][currentRotation];
+
+
+    // Draw the Shape
+
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].addClass("block");
+        });
+    }
+
+    //Undraw the Shape
+
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].removeClass("block");
+        });
+    }
+
+    // Move Tetromino Down a block every second
+    function moveDown() {
+        undraw();
+        currentPosition += width;
+        draw();
+        freeze();
+    }
+
+    // Move Tetromino Left a block and Prevent Collisions with shapes
+    function moveLeft() {
+        undraw();
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % gridWidth === width - 1)
+    }
+
+    // Stops the Tetromino once it reaches the end of the grid
+
+    function freeze() {
+        
+    }
+
+
 });
 
 function createGridDivs(width, height) {
